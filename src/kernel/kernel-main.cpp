b341387efee1b238495d6227b16d11548155261e
Nick K <nickklapal@gmail.com>
2025-04-12 21:09:32 -0400

    Working Serial / COM 1 Debug Port

--- src/kernel/kernel-main.cpp
+++ src/kernel/kernel-main.cpp
@@ -2,11 +2,23 @@
 /// kernel-main.cpp
 /// @breif This is the C/C++ entry of the kernel.
 
+#define cli asm volatile("cli")
+#define sti asm volatile("sti")
+
+// COM1
+extern "C" void printf(const char* f, ...);
+
 #include <stdint.h>
+
+#include "misc/serial.hpp"
 
 extern "C" void kernel_main(uint32_t addr, uint32_t magic)
 {
-    
+    cli;
+
+    Kernel::Misc::COM1_Init();
+
+    printf("Hello World\n");
 
     for(;;){
         asm volatile("hlt");
---
+++ src/kernel/misc/serial.hpp
@@ -0,0 +1,12 @@
+/// ----------
+/// serial.hpp
+/// @brief This file declares the needed function(s) to setup the COM1 serial port,
+/// aswell as debuging through said port.
+
+#pragma once
+
+namespace Kernel{
+    namespace Misc{
+        extern void COM1_Init();
+    }
+}
