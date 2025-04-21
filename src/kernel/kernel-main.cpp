7ff3c4ee3cd0acb269ec776cca9b88b135cbfc83
Nick K <nickklapal@gmail.com>
2025-04-21 14:55:43 -0400

    Added VGA Terminal

--- src/kernel/kernel-main.cpp
+++ src/kernel/kernel-main.cpp
@@ -9,18 +9,25 @@
 extern "C" void printf(const char* f, ...);
 
 #include <stdint.h>
+#include "../../multiboot2.h"
 
-#include "misc/serial.hpp"
+#include "misc/serial-com/serial-com.hpp"
+
+#include "misc/vga-terminal/vga-terminal.hpp"
 
 extern "C" void kernel_main(uint32_t addr, uint32_t magic)
 {
     cli;
 
-    Kernel::Misc::COM1_Init();
+    Kernel::Misc::Serial_Com_Init();
 
-    printf("Hello World\n");
+    Kernel::Misc::VGA_Terminal_Init();
 
-    for(;;){
-        asm volatile("hlt");
-    }
+    if(magic != MULTIBOOT2_BOOTLOADER_MAGIC)
+        goto halt;
+
+    halt:
+        for(;;){
+            asm volatile("hlt");
+        }
 }
