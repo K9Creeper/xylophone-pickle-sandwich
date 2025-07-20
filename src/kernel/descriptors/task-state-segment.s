97468170691f6468bc6c47d3ec37fc90292fa77f
Nick K <nickklapal@gmail.com>
2025-07-19 22:17:00 -0400

    Cleaned out Branch

--- src/kernel/descriptors/task-state-segment.s
+++
@@ -1,9 +0,0 @@
-### ----------------------
-### task-state-segment.s
-### @brief This file defines the functions needed to set up the Task State Segment.
-
-.global _tss_flush
-_tss_flush:
-    mov $0x28, %ax
-    ltr %ax
-    ret
