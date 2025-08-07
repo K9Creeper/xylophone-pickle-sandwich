ad4284fb2278e3ac2bfd89a946d76e17a3d9d602
Nick K <nickklapal@gmail.com>
2025-08-07 18:24:51 -0400

    Added task_queue, needs to intergrate thread safety.

---
+++ src/scheduling/task-queue.c
@@ -0,0 +1,104 @@
+/// ------------
+/// task-queue.c
+/// @brief This file defines the functions of the task queue handling.
+
+#include <scheduling/task-queue.h>
+
+void task_queue_init(task_queue_t *queue)
+{
+    if (queue == NULL)
+        return;
+
+    queue->_list = NULL;
+    queue->total = 0;
+}
+
+int task_queue_push(task_queue_t *queue, task_t *task)
+{
+    if (queue == NULL || task == NULL)
+        return -1;
+
+    task_t *current = queue->_list;
+    if (current == NULL)
+    {
+        queue->_list = task;
+        return 0;
+    }
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = task;
+    task->next = NULL;
+    queue->total++;
+
+    return 0;
+}
+
+int task_queue_add(task_queue_t *queue, task_t *task)
+{
+    if (queue == NULL)
+        return -1;
+
+    task->next = queue->_list;
+    queue->_list = task;
+
+    queue->total++;
+
+    return 0;
+}
+
+void task_queue_remove(task_queue_t *queue, task_t *task)
+{
+    if (queue == NULL || task == NULL || queue->_list == NULL)
+        return;
+
+    if (queue->_list == task)
+    {
+        queue->_list = task->next;
+        queue->total--;
+        return;
+    }
+
+    task_t *current = queue->_list;
+    while (current->next != NULL && current->next != task)
+    {
+        current = current->next;
+    }
+
+    if (current->next == NULL)
+    {
+        return;
+    }
+
+    current->next = task->next;
+    queue->total--;
+}
+
+task_t *task_queue_pop(task_queue_t *queue)
+{
+    if (queue == NULL || queue->_list == NULL)
+        return NULL;
+
+    task_t *front = NULL;
+
+    front = queue->_list;
+    queue->_list = front->next;
+
+    front->next = NULL;
+    front->prev = NULL;
+
+    return front;
+}
+
+task_t *task_queue_peek(task_queue_t *queue)
+{
+    if (queue == NULL || queue->_list == NULL)
+        return NULL;
+
+    task_t *front = NULL;
+
+    front = queue->_list;
+
+    return front;
+}
