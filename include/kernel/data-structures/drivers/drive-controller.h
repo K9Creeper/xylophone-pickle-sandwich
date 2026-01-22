e8ef62fd41ed956c02765cedf412939850a80091
Nick K <nickklapal@gmail.com>
2026-01-22 18:10:21 -0500

    Setup basic interface for a drive controller

--- include/kernel/data-structures/drivers/drive-controller.h
+++ include/kernel/data-structures/drivers/drive-controller.h
@@ -4,4 +4,33 @@
 #ifndef DRIVER_DRIVE_CONTROLLER_H
 #define DRIVER_DRIVE_CONTROLLER_H
 
+#include <stdint.h>
+
+typedef int32_t (*drive_read_t)(
+    void *impl,
+    uint64_t lba,
+    uint32_t sector_count,
+    void *buffer
+);
+
+typedef int32_t (*drive_write_t)(
+    void *impl,
+    uint64_t lba,
+    uint32_t sector_count,
+    const void *buffer
+);
+
+typedef struct drive_controller_ops_s {
+    drive_read_t  read;
+    drive_write_t write;
+    uint32_t (*sector_size)(void *impl);
+    uint64_t (*sector_count)(void *impl);
+} drive_controller_ops_t;
+
+typedef struct drive_controller_s {
+    const char *name;
+    void *impl;
+    const drive_controller_ops_t *ops;
+} drive_controller_t;
+
 #endif
