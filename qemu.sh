5d9b5b19d0bff23521a92013a7b3d4b97ade3310
Nick K <nickklapal@gmail.com>
2026-01-24 20:01:02 -0500

    cleaned up qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,6 @@
 # sudo apt install qemu-system-i386 qemu-utils
 qemu-system-i386 -m 512M \
     -drive file=build/os.img,format=raw,if=ide \
-    -boot c -vga std -serial file:debug-log.txt
+    -boot c \
+    -vga std \
+    -serial file:debug-log.txt
